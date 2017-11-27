c93033144aa6a9390f5059564c302dbe87b0abc3
Adam <adam.hall@sky.uk>
2017-11-27 11:37:44 +0000

    made request constants more readable

--- src/api/v1/requests/requests.js
+++ src/api/v1/requests/requests.js
@@ -1,9 +1,24 @@
 import constants from './constants';
 
 export default {
-    transactions: { request: `transactions?account_id=${constants.ACCOUNT_ID}&limit=10`, safeLog: 'transactions?account_id=$account_id&limit=10' },
-    transactionById: { request: 'transactions/tx_00009E3NxX9vhvVVfIP9UX', safeLog: 'transactions/$transaction_id' },
-    accounts: { request: 'accounts', safeLog: 'accounts' },
-    balance: { request: `balance?account_id=${constants.ACCOUNT_ID}`, safeLog: 'balance?account_id=$account_id' },
-    whoami: { request: 'ping/whoami', safeLog: 'ping/whoami' },
+    transactions: {
+        request: `transactions?account_id=${constants.ACCOUNT_ID}&limit=10`,
+        safeLog: 'transactions?account_id=$account_id&limit=10',
+    },
+    transactionById: {
+        request: 'transactions/tx_00009E3NxX9vhvVVfIP9UX',
+        safeLog: 'transactions/$transaction_id',
+    },
+    accounts: {
+        request: 'accounts',
+        safeLog: 'accounts',
+    },
+    balance: {
+        request: `balance?account_id=${constants.ACCOUNT_ID}`,
+        safeLog: 'balance?account_id=$account_id',
+    },
+    whoami: {
+        request: 'ping/whoami',
+        safeLog: 'ping/whoami',
+    },
 };
